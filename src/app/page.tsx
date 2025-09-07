e32effb38516f11ee7c18a10f453718d84d321d1
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-09-07 21:09:47 +0800

    prisma model done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,3 +13,13 @@ export default page
 //prisma install
 //npm i prisma 
 //npx prisma init --datasource-provider mongodb 
+/*
+Next steps:
+1. Set the DATABASE_URL in the .env file to point to your existing database. If your database has no collections yet, read https://pris.ly/d/getting-started
+2. Define models in the schema.prisma file.
+3. Run prisma generate to generate the Prisma Client. You can then start querying your database.
+4. Tip: Explore how you can extend the ORM with scalable connection pooling, global caching, and a managed serverless Postgres database. Read: https://pris.ly/cli/beyond-orm
+
+More information in our documentation:
+https://pris.ly/d/getting-started
+*/
