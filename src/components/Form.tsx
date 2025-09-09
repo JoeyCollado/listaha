b2291356352468a12d20a45138693ca40882e682
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-09-09 11:27:33 +0800

    Form component done

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,10 +1,26 @@
-import { formProps } from '@/types'
-import React from 'react'
+"use client";
+import { formProps } from "@/types"; //types
+import React from "react";
+import { useRef } from "react";
 
-const Form = ({children, action, className, onSubmit}: formProps) => {
+const Form = ({ children, action, className, onSubmit }: formProps) => {
+  //props = inorder to use props you need to define the propTypes
+
+  const ref = useRef<HTMLFormElement>(null);
+
   return (
-    <form>{children}</form>
-  )
-}
+    //since this file is client we need to use action to use server actions
+    <form
+      action={async (formData) => {
+        await action(formData);
+        ref.current?.reset(); //reset formdata
+      }}
+      onSubmit={onSubmit}
+      ref={ref}
+    >
+      {children}
+    </form>
+  );
+};
 
-export default Form
+export default Form;
