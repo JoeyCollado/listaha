103ee670ffc4d0db86f0fb56e8773cad60c587e0
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-09-09 11:12:44 +0800

    initial ui done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,14 @@ const page = () => {
   return (
     <div className='w-screen py-20 flex justify-center flex-col items-center'>
       <span className='text-4xl font-extrabold uppercase'>Listaha App</span>
+      <h1 className='text-5xl font-extrabold uppercase mb-5 text-center'>
+        <span className='lowercase'>w</span>Server Actions
+      </h1>
+
+      <div className='flex justify-center flex-col items-center'>
+        {/* input todo */}
+        {/* map todos */}
+      </div>
     </div>
   )
 }
