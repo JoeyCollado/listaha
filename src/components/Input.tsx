5f357ad7c563f6e1424f0b83afe683df0d096011
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-09-09 11:19:42 +0800

    input styled

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -10,10 +10,10 @@ export interface inputProps{
 }
 */
 
-const Input = ({name, type, placeholder, value}: inputProps) => {
+const Input = ({name, type, placeholder, value}: inputProps) => { //props
   return (
     <div>
-      <input className='' title='input'/>
+      <input name={name} type={type} placeholder={placeholder} value={value} className='bloc w-full p-4 mx-2 border rounded-lg text-base bg-gray-700 border-gray-600 placeholder:gray-400 text-white' title='input'/>
     </div>
   )
 }
