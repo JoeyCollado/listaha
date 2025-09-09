fed45626091c3b625eea48c2eec8a1763f98f61f
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-09-09 11:17:42 +0800

    types

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { inputProps } from '@/types' //types in a separate file, along with this you can mix in same file like the below
 
-const Input = () => {
+/*
+export interface inputProps{
+    name: string;
+    type: string;
+    placeholder?: string;
+    value?: string;
+}
+*/
+
+const Input = ({name, type, placeholder, value}: inputProps) => {
   return (
     <div>
-      
+      <input className='' title='input'/>
     </div>
   )
 }
---
+++ src/types/index.tsx
@@ -0,0 +1,6 @@
+export interface inputProps{
+    name: string;
+    type: string;
+    placeholder?: string;
+    value?: string;
+}
