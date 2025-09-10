3c0581702c4e0f56d35cb364011ab00db2ce7f75
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-09-10 10:54:18 +0800

    button styling set

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,14 @@
 import { buttonProps } from '@/types'
+import clsx from 'clsx'
 import React from 'react'
 
 const Button = ({type, text, onClick, actionButton, bgColor, ...props}: buttonProps) => {
   return (
     <div>
-      <button onClick={onClick} type={type} className=''>
+      <button onClick={onClick} type={type} className={clsx(
+        actionButton && 'text-white hover:bg-blue-800 focus:ring-4 font-medium founded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none',
+        `${bgColor} hover:${bgColor} font-medium founded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none`
+      )}>
         {text}
       </button>
     </div>
@@ -12,3 +16,5 @@ const Button = ({type, text, onClick, actionButton, bgColor, ...props}: buttonPr
 }
 
 export default Button
+
+//npm i clsx react-icons
