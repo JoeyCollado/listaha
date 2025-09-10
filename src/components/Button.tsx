6dc6ff3e7250c0e5e0305822aaf75adb88cf412a
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-09-10 10:50:51 +0800

    installing npm i clsx react-icons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,7 +4,9 @@ import React from 'react'
 const Button = ({type, text, onClick, actionButton, bgColor, ...props}: buttonProps) => {
   return (
     <div>
-      
+      <button onClick={onClick} type={type} className=''>
+        {text}
+      </button>
     </div>
   )
 }
