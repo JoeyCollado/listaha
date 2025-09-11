f31fda68fc79e8cca3ff01a0301737968d587876
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-09-11 12:06:48 +0800

    initial styling

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import Input from "./Input";
+import Form from "./Form";
+import Button from "./Button";
 
 const Todos = () => {
   return (
     <div>
-      
+      <Form>
+        <div className="flex gap-2">
+          <Input name="input" type="text" placeholder="Add Todo Here">
+            <Button type="submit" text="Add" bgColor="bg-blue-600"/>
+          </Input>
+        </div>
+      </Form>
     </div>
-  )
-}
+  );
+};
 
-export default Todos
+export default Todos;
