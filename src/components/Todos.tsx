9907a35ad7853f5e706fc244c2296dc5fee85970
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-09-12 10:59:14 +0800

    fixed error and add todo function done

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,14 +1,17 @@
 import Form from "./Form";
 import Input from "./Input";
 import Button from "./Button";
+import { createTodo } from "@/actions";
 
-
+//how this works
+//when we click the button the form will start working
+//and after that the serveraction createTodo will grab the input via the formData which is the same props we use on our actions file, then we use the prisma to inject data to our mongodb
 
 
 const AddTodo = () => {
   return (
     <div>
-      <Form>
+      <Form action={createTodo}>
         <div className="flex gap-4 items-center">
           <Input name="input" type="text" placeholder="Add Todo Here..." />
           <Button type="submit" text="Add" bgColor="bg-blue-600" />
--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,6 +1,9 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/generated/prisma";
 
 export const prisma = new PrismaClient();
 
 //this file control prisma to mongodb
 //insert,edit,delete data
+
+//npm install @prisma/client
+//npx prisma generate
