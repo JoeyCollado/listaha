a05bb8bdcc0ba9c0f38ca9a2bceaa88a7d6f2c60
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-09-12 10:17:59 +0800

    Add todo

--- src/actions/index.tsx
+++ src/actions/index.tsx
@@ -1,8 +1,21 @@
 'use server';
 //server actions
 import {prisma} from "@/utils/prisma"
+import { revalidatePath } from "next/cache";
 
 //add todo
 export async function createTodo(formData: FormData){
-  
+  const input = formData.get('input') as string; //get data from form input
+
+  if(!input.trim()){
+    return;
+  }
+  await prisma.todo.create({
+    data: {
+        title: input,
+    }
+  })
+
+  //refresh path
+  revalidatePath('/');
 }
