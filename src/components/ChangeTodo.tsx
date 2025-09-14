a42e7e35134f5b9f374f57f43a3fa1e3a3437a33
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-09-14 11:54:28 +0800

    Savepoint

--- src/components/ChangeTodo.tsx
+++ src/components/ChangeTodo.tsx
@@ -8,7 +8,7 @@ import { FaCheck } from 'react-icons/fa';
 
 const ChangeTodo = ({todo}: todoProps) => {
   return (
-    <Form action={}>
+    <Form action={actions.changeStatus}>
         <Input name='inputId' value={todo.id} type='hidden'></Input>
         <Button type='submit' text={<FaCheck/>} actionButton bgColor={todo.isCompleted? 'bg-green-400' : 'bg-blue-500'}></Button> {/* if completed checkmark is green if not blue */}
     </Form>
