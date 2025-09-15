1161fa78f911831601ac57d3c611cf7a2a680544
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-09-15 13:44:39 +0800

    handle edit state

--- src/components/EditTodo.tsx
+++ src/components/EditTodo.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+'use client';
+import React, { useState } from 'react'
+import Button from './Button';
 
 const EditTodo = () => {
+    const [editTodo, setEditTodo] = useState(false);
+
+    const handleEdit = () => {
+        setEditTodo(!editTodo); //when btn click set edit todo state to the opposite of current editTodo state, if its false turn true vice versa
+    }
   return (
-    <div>
-      
+    <div className='flex gap-5 items-center'>
+      <Button onClick={handleEdit}/>
     </div>
   )
 }
