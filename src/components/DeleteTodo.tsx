e7c34479760f3ab20cb4dac32c44014aec05327d
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-09-18 09:57:30 +0800

    delete todo ui

--- src/components/DeleteTodo.tsx
+++ src/components/DeleteTodo.tsx
@@ -1,10 +1,16 @@
+import { todoProps } from '@/types'
 import React from 'react'
+import Form from './Form'
+import Input from './Input'
+import Button from './Button'
+import { FaTrash } from 'react-icons/fa'
 
-const DeleteTodo = () => {
+const DeleteTodo = ({todo}: {todo: todoProps}) => {
   return (
-    <div>
-      
-    </div>
+    <Form>
+        <Input type='hidden' name='inputId' value={todo.id}></Input>
+        <Button actionButton type='submit' bgColor='bg-red-400' text={<FaTrash/>}></Button>
+    </Form>
   )
 }
 
