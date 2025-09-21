288b0e5a5aa01f8a4b1586ed14b10509384ac3b0
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-09-21 11:40:02 +0800

    fix vercel issues

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,6 +1,14 @@
-import { PrismaClient } from "@/generated/prisma";
+import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({
+  log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+});
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 //this file control prisma to mongodb
 //insert,edit,delete data
